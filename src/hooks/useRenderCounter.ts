8d9bf7f844aed53e41e327cd47cf2a689c313916
pmacman <266302713+pmacman@users.noreply.github.com>
2026-06-25 19:56:13 -0400

    Updated useRenderCounter hook

--- src/hooks/useRenderCounter.ts
+++ src/hooks/useRenderCounter.ts
@@ -1,5 +1,8 @@
+import { useRef } from 'react';
+
 /**
- * A custom React hook that logs each render of a component instance to the browser console.
+ * A custom React hook that tracks, increments, and logs the render count
+ * of a component instance to the browser console.
  * @remarks
  * This hook will only log when running in a development environment (`import.meta.env.DEV === true`)
  * and if present, requires your build system's global log flag (`VITE_ENABLE_RENDER_LOGS`) to be set to `'true'`.
@@ -17,12 +20,32 @@
  * ```
  */
 export function useRenderCounter(componentName: string, isDebugging: boolean = true): void {
+  const renderCount = useRef(0);
+
+  // Vite compatible:
   const isDev = import.meta.env.DEV;
+  // Node compatible:
+  // const isDev = process.env.NODE_ENV === 'development';
+
+  // Vite compatible:
   const globalLogEnabled = import.meta.env.VITE_ENABLE_RENDER_LOGS === 'true';
+  // Create React App compatible:
+  // const globalLogEnabled = process.env.REACT_APP_ENABLE_RENDER_LOGS === 'true'
 
   if (!isDev || !globalLogEnabled || !isDebugging) {
     return;
   }
 
-  console.count(`[Render Trace] ${componentName}`);
+  renderCount.current++;
+
+  // CUSTOMIZE MESSAGE HERE
+  const message = `%c[Render Trace] %c${componentName} %chas rendered. Count: %c${renderCount.current}`;
+
+  // CUSTOMIZE STYLES HERE
+  const labelStyle = 'color: #70a1ff; font-weight: bold;';
+  const componentStyle = 'color: #ff4757; font-weight: bold;';
+  const normalStyle = 'color: inherit;';
+  const countStyle = 'color: #2ed573; font-weight: bold;';
+
+  console.log(message, labelStyle, componentStyle, normalStyle, countStyle);
 }
